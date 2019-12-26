1fb0642cfcb1a14caa37791ad01d0823d359a83b
lucky863 <laxmipalkant01@gmail.com>
2019-12-26 12:39:30 +0530

    first commit

---
+++ sdkrepo/src/main/java/com/videozone/sdkrepo/SdkDebug.java
@@ -0,0 +1,16 @@
+package com.videozone.sdkrepo;
+
+import android.util.Log;
+
+public class SdkDebug {
+
+    private static final String TAG = "Darktheft";
+
+    public static void d(String message){
+        Log.d(TAG,message);
+    }
+
+    public static void lucky(String message){
+        Log.d(TAG,message);
+    }
+}
